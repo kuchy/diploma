156db2b2e33807cf40bd82b18a12e44f5eed9a6b
Martin Kuchynar <matokuchy@gmail.com>
2016-03-07 22:35:17 +0100

    Fixing bugs && changing adaptive thresholding to preserve exact number of regions.

--- src/models/prefinal.m
+++ src/models/prefinal.m
@@ -6,9 +6,10 @@ debug = false;
 % estimation
 
 %% Object flow estimation 
+% TODO do this only if there if bug resulution!
 % % reduce image by gausian pyramid
-CurrentFrameOrig = impyramid(CurrentFrameOrig, 'reduce');
-CurrentFrameOrig = impyramid(CurrentFrameOrig, 'reduce');
+% CurrentFrameOrig = impyramid(CurrentFrameOrig, 'reduce');
+% CurrentFrameOrig = impyramid(CurrentFrameOrig, 'reduce');
 
 hsize=[5 5];
 sigma=1;
@@ -16,10 +17,11 @@ h = fspecial('gaussian',hsize,sigma);
 
 persistent PreviousFrame;
 if isempty(PreviousFrame)
-    PreviousFrame = impyramid(PreviousFrameOrig, 'reduce');
-    PreviousFrame = impyramid(PreviousFrame, 'reduce');
-    PreviousFrame= imfilter(rgb2gray(PreviousFrame),h,'replicate');
+%     PreviousFrame = impyramid(PreviousFrameOrig, 'reduce');
+%     PreviousFrame = impyramid(PreviousFrame, 'reduce');
+    
 end
+PreviousFrame= imfilter(rgb2gray(PreviousFrameOrig),h,'replicate');
 CurrentFrame=imfilter(rgb2gray(CurrentFrameOrig),h,'replicate');
 [height, width]=size(PreviousFrame);
 
@@ -46,7 +48,6 @@ for i=iterations
 end
 % TODO BETTER preserve v and u
 img = TempU + TempV;
-
 persistent TTL;
 persistent memory;
 memoryRange = 24;
@@ -57,90 +58,106 @@ if isempty(TTL)
 end
 
 %% Marging regions by velocity && distance
-objectFlowThreshold = 0.2;
 
-% thresholding for regioprops
-img = abs(img) > objectFlowThreshold;
+% % takes only top 50% of moovement
+% [pixelCounts grayLevels] = imhist(img);
+% cdf = cumsum(pixelCounts) / sum(pixelCounts);
+% thresholdIndex = find(cdf > 0.8, 1, 'first');
+% objectFlowThreshold = grayLevels(thresholdIndex);
+% %if ( objectFlowThreshold > 0.8)
+% %    objectFlowThreshold = 0.8;
+% %end
 
+% thresholding regioprops to have less than 400 regions
+objectFlowThreshold = graythresh(img);
+img2 = abs(img) > objectFlowThreshold;
 cc = bwconncomp(img,8);
-rp = regionprops(img, 'Area', 'PixelList','centroid','Extrema', 'PixelIdxList');
+cc.NumObjects
+while (cc.NumObjects>200)
+    objectFlowThreshold = objectFlowThreshold+objectFlowThreshold/10;
+    img2 = abs(img) > objectFlowThreshold;
+    cc = bwconncomp(img2,8);
+    cc.NumObjects
+end
+img = img2;
+cc.NumObjects
 
-% if have some moving regions
-if (cc.NumObjects > 2)
-    % Sorting by size, largest first
-%      [~, ind] = sort([rp.Area], 'descend');
-%      rp=rp(ind);
-%     try
-%         finding shifting by region
-        horizontalAverage = zeros(cc.NumObjects,1);
-        verticalAverage = zeros(cc.NumObjects,1);
-        
-        for regionIndex=1:cc.NumObjects             
-            horizontalAverage(regionIndex) = max(TempV(rp(regionIndex).PixelIdxList));
-            verticalAverage(regionIndex) = max(TempU(rp(regionIndex).PixelIdxList));
-        end
-        
-%         create coocurrance matrix of distance of regions
-        distanceAverage = zeros(cc.NumObjects,cc.NumObjects);
-        for regionIndexOne=1:cc.NumObjects
-            for regionIndexTwo=round(cc.NumObjects/2)+1:-1:(regionIndexOne+1)
-               extrema1 = rp(regionIndexOne).Extrema;
-               extrema2 = rp(regionIndexTwo).Extrema;              
-%              calculate distance
-               min = 999999999999;
-               for j=1:8
-                   corner1 = extrema1(j,:);
-                   for k=1:8
-                       corner2 = extrema2(k,:);
-                       distance = sqrt( (corner2(1,1)-corner1(1,1))^2 + (corner2(1,2)-corner1(1,2))^2 );                       
-                       if distance<min
-                           min = distance;
+    cc = bwconncomp(img,8);
+    rp = regionprops(img, 'Area', 'PixelList','centroid','Extrema', 'PixelIdxList');
+
+%     disp(cc.NumObjects);
+    % if have some moving regions
+    if (cc.NumObjects > 2)
+   
+            horizontalAverage = zeros(cc.NumObjects,1);
+            verticalAverage = zeros(cc.NumObjects,1);
+
+    %         finding shifting by region
+    %         create coocurrance matrix of distance of regions
+            distanceAverage = zeros(cc.NumObjects,cc.NumObjects);
+            for regionIndexOne=1:cc.NumObjects
+                horizontalAverage(regionIndexOne) = max(TempV(rp(regionIndexOne).PixelIdxList));
+                verticalAverage(regionIndexOne) = max(TempU(rp(regionIndexOne).PixelIdxList));               
+                for regionIndexTwo=regionIndexOne+1:1:cc.NumObjects
+                   extrema1 = rp(regionIndexOne).Extrema;
+                   extrema2 = rp(regionIndexTwo).Extrema;              
+    %              calculate distance
+                   min = 999999999999;
+                   for j=1:8
+                       corner1 = extrema1(j,:);
+                       for k=1:8
+                           corner2 = extrema2(k,:);
+                           distance = sqrt( (corner2(1,1)-corner1(1,1))^2 + (corner2(1,2)-corner1(1,2))^2 );                       
+                           if distance<min
+                               min = distance;
+                           end
                        end
                    end
-               end
-              
-             distanceAverage(regionIndexOne, regionIndexTwo) = min;
-             distanceAverage(regionIndexTwo, regionIndexOne) = min;                
-            end
-        end       
-        img = mat2gray(img);
-        
-%         constants
-        velocityTreshold = 0.1;
-        distanceTreshold = 10;
-        
-%       Marge all regions with save directions and
-        for regionIndexOne=1:cc.NumObjects
-            for regionIndexTwo=regionIndexOne+1:1:cc.NumObjects
-                horizontalCondition = abs(horizontalAverage(regionIndexOne) - horizontalAverage(regionIndexTwo)) < velocityTreshold;
-                verticalCondition = abs(verticalAverage(regionIndexOne) - verticalAverage(regionIndexTwo)) < velocityTreshold;
-                positionCondition = distanceAverage(regionIndexOne,regionIndexTwo) < distanceTreshold;
-                if (verticalCondition && horizontalCondition && positionCondition)
-                    if debug
-                        disp(strcat(int2str(regionIndexOne), '-margeWith-',int2str(regionIndexTwo)));
-                    end
-                    y =[rp(regionIndexOne).PixelList(:,2);rp(regionIndexTwo).PixelList(:,2)];
-                    x =[rp(regionIndexOne).PixelList(:,1);rp(regionIndexTwo).PixelList(:,1)];
-                    
-%                   too few points to calculate convex hull
-                    if size(unique(y),1) < 3 || size(unique(x),1) < 3
-                        continue;
-                    end                   
-                    
-                    try
-                        k = convhull(x,y);
-                        x_hull = x(k);
-                        y_hull = y(k);
-                        mask = poly2mask(x_hull, y_hull, size(img,1),size(img,2));
-                        img = img + mask;
-                    catch
+
+                 distanceAverage(regionIndexOne, regionIndexTwo) = min;
+                 distanceAverage(regionIndexTwo, regionIndexOne) = min;                
+                end
+            end       
+            img = mat2gray(img);
+
+    %         constants
+            velocityTreshold = 0.5;
+            distanceTreshold = 30;
+
+    %       Marge all regions with save directions and
+            for regionIndexOne=1:cc.NumObjects
+                for regionIndexTwo=regionIndexOne+1:1:cc.NumObjects
+                    horizontalCondition = abs(horizontalAverage(regionIndexOne) - horizontalAverage(regionIndexTwo)) < velocityTreshold;
+                    verticalCondition = abs(verticalAverage(regionIndexOne) - verticalAverage(regionIndexTwo)) < velocityTreshold;
+                    positionCondition = distanceAverage(regionIndexOne,regionIndexTwo) < distanceTreshold;
+                    if (verticalCondition && horizontalCondition && positionCondition)
                         if debug
-                            disp('colinear points');
+                            disp(strcat(int2str(regionIndexOne), '-margeWith-',int2str(regionIndexTwo)));
                         end
-                    end 
+                        y =[rp(regionIndexOne).PixelList(:,2);rp(regionIndexTwo).PixelList(:,2)];
+                        x =[rp(regionIndexOne).PixelList(:,1);rp(regionIndexTwo).PixelList(:,1)];
+
+    %                   too few points to calculate convex hull
+                        if size(unique(y),1) < 3 || size(unique(x),1) < 3
+                            continue;
+                        end                   
+
+                        try
+                            k = convhull(x,y);
+                            x_hull = x(k);
+                            y_hull = y(k);
+                            mask = poly2mask(x_hull, y_hull, size(img,1),size(img,2));
+                            img = img + mask;
+                        catch
+                            if debug
+                                disp('colinear points');
+                            end
+                        end 
+                    end
                 end
             end
-        end    
+
+    end
     
 %   treshold matrix
     img = img > 0;
@@ -175,6 +192,14 @@ if (cc.NumObjects > 2)
     staticFeatures = spectralResidual(CurrentFrameOrig);
     percentage = ((sum(img(:) > 0))/numel(img));
     
-    img = (img .* (1-percentage)) + (staticFeatures .* percentage);
+    if (percentage < 0.05)
+        % no dimanic features use only static
+        img = staticFeatures;        
+        disp('only static');
+    else
+        img = (img .* (1.5-percentage)) + (staticFeatures .* percentage);
+        disp('mixing');
+    end
+    
     disp(percentage);
 end
