d81afb566d7790f69d8450656ac80e37a41701c5
Martin Kuchynar <matokuchy@gmail.com>
2016-04-05 00:12:12 +0200

    Updating modal to actual version.

--- src/models/prefinal.m
+++ src/models/prefinal.m
@@ -2,25 +2,14 @@ function img= hornSchunckNew( CurrentFrameOrig, PreviousFrameOrig )
 
 debug = false;
 
-% detect if camera is static and use median filtration else use object flow
+%  TODO detect if camera is static and use median filtration else use object flow
 % estimation
-
-%% Object flow estimation 
-% TODO do this only if there if bug resulution!
-% % reduce image by gausian pyramid
-% CurrentFrameOrig = impyramid(CurrentFrameOrig, 'reduce');
-% CurrentFrameOrig = impyramid(CurrentFrameOrig, 'reduce');
 
 hsize=[5 5];
 sigma=1;
 h = fspecial('gaussian',hsize,sigma);
-
-persistent PreviousFrame;
-if isempty(PreviousFrame)
-%     PreviousFrame = impyramid(PreviousFrameOrig, 'reduce');
-%     PreviousFrame = impyramid(PreviousFrame, 'reduce');
     
-end
+% end
 PreviousFrame= imfilter(rgb2gray(PreviousFrameOrig),h,'replicate');
 CurrentFrame=imfilter(rgb2gray(CurrentFrameOrig),h,'replicate');
 [height, width]=size(PreviousFrame);
@@ -46,43 +35,19 @@ for i=iterations
     TempU= ubar - ( TempEx.* ( ( TempEx.* ubar ) + ( TempEy.* vbar ) + TempEt ) ) ./ ( alpha^2 + TempEx.^2 + TempEy.^2);
     TempV= vbar - ( TempEy.* ( ( TempEx.* ubar ) + ( TempEy.* vbar ) + TempEt ) ) ./ ( alpha^2 + TempEx.^2 + TempEy.^2);
 end
-% TODO BETTER preserve v and u
-img = TempU + TempV;
-persistent TTL;
-persistent memory;
-memoryRange = 24;
-persistent frameNumber;
-if isempty(TTL)
-    TTL=ones(size(img));
-    frameNumber = 0;    
-end
-
-%% Marging regions by velocity && distance
-
-% % takes only top 50% of moovement
-% [pixelCounts grayLevels] = imhist(img);
-% cdf = cumsum(pixelCounts) / sum(pixelCounts);
-% thresholdIndex = find(cdf > 0.8, 1, 'first');
-% objectFlowThreshold = grayLevels(thresholdIndex);
-% %if ( objectFlowThreshold > 0.8)
-% %    objectFlowThreshold = 0.8;
-% %end
 
 % thresholding regioprops to have less than 400 regions
 objectFlowThreshold = graythresh(img);
 img2 = abs(img) > objectFlowThreshold;
 cc = bwconncomp(img,8);
-cc.NumObjects
 while (cc.NumObjects>200)
     objectFlowThreshold = objectFlowThreshold+objectFlowThreshold/10;
     img2 = abs(img) > objectFlowThreshold;
     cc = bwconncomp(img2,8);
-    cc.NumObjects
 end
+% use trasholded image
 img = img2;
-cc.NumObjects
 
-    cc = bwconncomp(img,8);
     rp = regionprops(img, 'Area', 'PixelList','centroid','Extrema', 'PixelIdxList');
 
 %     disp(cc.NumObjects);
@@ -91,7 +56,6 @@ cc.NumObjects
    
             horizontalAverage = zeros(cc.NumObjects,1);
             verticalAverage = zeros(cc.NumObjects,1);
-
     %         finding shifting by region
     %         create coocurrance matrix of distance of regions
             distanceAverage = zeros(cc.NumObjects,cc.NumObjects);
@@ -122,7 +86,7 @@ cc.NumObjects
 
     %         constants
             velocityTreshold = 0.5;
-            distanceTreshold = 30;
+            distanceTreshold = 50;
 
     %       Marge all regions with save directions and
             for regionIndexOne=1:cc.NumObjects
@@ -162,44 +126,27 @@ cc.NumObjects
 %   treshold matrix
     img = img > 0;
     
-    % apply ttl matrix
-    img = img .* TTL;
-
-%   calculate coeficients
-    memoryCoeficient = 0.2;
-
-    if size(memory,2)>=20
-        TTL = ones(size(img));
-        for ii = 1:size(memory,2)
-            TTL = TTL - (memory{ii}/size(memory,2))*memoryCoeficient;
-        end
-    end
-    
-    
-%   TODO: 24frames memory
-    modFrame = mod(frameNumber, memoryRange)+1;
-    memory{modFrame} = img;
-    frameNumber = frameNumber + 1;
-    
 %   Apply gausian filter
     hsize=[10 10];
     sigma=1;
     h = fspecial('gaussian',hsize,sigma);
     
     img = imfilter(img, h);
-    PreviousFrame = CurrentFrame;
+%     PreviousFrame = CurrentFrame;
     
     staticFeatures = spectralResidual(CurrentFrameOrig);
     percentage = ((sum(img(:) > 0))/numel(img));
     
-    if (percentage < 0.05)
+    if (percentage < 0.01)
         % no dimanic features use only static
         img = staticFeatures;        
-        disp('only static');
+%         disp('only static');
     else
         img = (img .* (1.5-percentage)) + (staticFeatures .* percentage);
-        disp('mixing');
+%         disp('mixing');
     end
     
-    disp(percentage);
+%     center = centerModel(CurrentFrameOrig);
+%     img = (uint8(255 * img) + center) /2;
+
 end
