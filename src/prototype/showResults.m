fc019745d38dac02cc32158fe180282f85c2dbc4
Martin Kuchynar <matokuchy@gmail.com>
2016-04-22 00:21:29 +0200

    Adding new type of graph.

--- src/prototype/showResults.m
+++ src/prototype/showResults.m
@@ -1,9 +1,8 @@
-function showResults (dataset)
+function showResults (dataset, numOfVideos)
     path = strcat('results/',dataset,'/');
     listing = dir(path{1});
     tests = {};
-    
-    numOfVideos = 24;
+   
 
     %% remove system files
     testNumber = 1;
@@ -27,6 +26,15 @@ function showResults (dataset)
 
         data = load(fullfile(path{1},char(tests(i))));
         result.(modelName).data(videoNumber) = data;
+        
+        notNaN = ~isnan(data.AUROC_score);
+        data.AUROC_score(~notNaN) = 0;
+        
+        notNaN = ~isnan(data.NSS_score);
+        data.NSS_score(~notNaN) = 0;
+        
+        notNaN = ~isnan(data.KLDIV_score);
+        data.KLDIV_score(~notNaN) = 0;
 
     %     calculate and save frame average
         average.AUROC_score = mean(data.AUROC_score);
@@ -35,10 +43,22 @@ function showResults (dataset)
         result.(modelName).frameMean(videoNumber) = average;
 
     %     refresh overall average
+        a=mean([result.(modelName).frameMean(:).AUROC_score]);
         result.(modelName).mean.AUROC_score = mean([result.(modelName).frameMean(:).AUROC_score]);
         result.(modelName).mean.KLDIV_score = mean([result.(modelName).frameMean(:).KLDIV_score]);
         result.(modelName).mean.NSS_score = mean([result.(modelName).frameMean(:).NSS_score]);
     end
+
+    %% MIN/MAX definitions
+    AUC_max = 1;
+    AUC_min = 0.45;
+    AUC_random = [0.5 0.5];
+    
+    NSS_max = 3;
+    NSS_min = 0;
+    
+    KLDIV_max = 2.0;
+    KLDIV_min = 1.4;
 
     %% plot AUROC_score
     i = 1;
@@ -53,12 +73,15 @@ function showResults (dataset)
     subplot(3, 1, 1)
     title('Results for selected Dataset');
     bar(1:numOfVideos, graph', 1)
-    axis([0 numOfVideos+5 0 1])
+    hold on
+    plot(xlim, AUC_random, 'color', [0.5 0.5 0.5])
+    axis([0 numOfVideos+6 AUC_min AUC_max])
     set(gca, 'XTick', 1:numOfVideos)
     xlabel('Video');
     ylabel('Score of AUROC_score');
     legend(fieldnames(result)');
-    clear data
+    clear data;
+    clear graph;
 
     %% plot KLDIV_score
     i = 1;
@@ -72,12 +95,13 @@ function showResults (dataset)
     subplot(3, 1, 2)
     title('Results for selected Dataset');
     bar(1:numOfVideos, graph', 1)
-    axis([0 numOfVideos+5 0 3])
+    axis([0 numOfVideos+6 KLDIV_min KLDIV_max])
     set(gca, 'XTick', 1:numOfVideos)
     xlabel('Video');
     ylabel('Score of KLDIV_score');
     legend(fieldnames(result)');
-    clear data
+    clear data;
+    clear graph;
 
     %% plot NSS_score
     i = 1;
@@ -93,9 +117,105 @@ function showResults (dataset)
     subplot(3, 1, 3)
     title('Results for selected Dataset');
     bar(1:numOfVideos, graph', 1)
-    axis([0 numOfVideos+5 -0.8 3])
+    axis([0 numOfVideos+6 NSS_min NSS_max])
     set(gca, 'XTick', 1:numOfVideos)
     xlabel('Video');
     ylabel('Score of NSS_score');
     legend(fieldnames(result)');
+    clear data;
+    clear graph;
+    
+    %% plot global result AUCROC
+    i = 1;
+    for model = fieldnames(result)'   
+        modelName = char(model);
+        data= extractfield(result.(modelName).mean, 'AUROC_score');    
+        graph(i) = data;
+        i = i+1;
+    end
+    
+    figure
+    subplot(3, 1, 1)
+    title('Results for selected Dataset');
+    bar(1:i-1, graph', 1)
+    hold on
+    plot(xlim, AUC_random, 'color', [0.5 0.5 0.5])
+    axis([0 i AUC_min AUC_max])
+    set(gca, 'XTick', 1:i-1, 'XTickLabel', fieldnames(result))
+    ylabel('AUROC');
+    clear data;
+    clear graph;
+    
+    %% plot global result KLDIV
+    i = 1;
+    for model = fieldnames(result)'   
+        modelName = char(model);
+        data= extractfield(result.(modelName).mean, 'KLDIV_score');    
+        graph(i) = data;
+        i = i+1;
+    end
+    
+    subplot(3, 1, 2)
+    title('Results for selected Dataset');
+    bar(1:i-1, graph', 1)
+    axis([0 i KLDIV_min KLDIV_max])
+    set(gca, 'XTick', 1:i-1, 'XTickLabel', fieldnames(result))
+    ylabel('KLDIV');
+    clear data;
+    clear graph;
+    
+    %% plot global result NSS
+    i = 1;
+    for model = fieldnames(result)'   
+        modelName = char(model);
+        data= extractfield(result.(modelName).mean, 'NSS_score');    
+        graph(i) = data;
+        i = i+1;
+    end
+    
+    subplot(3, 1, 3)
+    title('Results for selected Dataset');
+    bar(1:i-1, graph', 1)
+    axis([0 i NSS_min NSS_max])
+    set(gca, 'XTick', 1:i-1, 'XTickLabel', fieldnames(result))
+    ylabel('NSS');
+    clear data;
+    clear graph;
+    
+    %% plot only our model by video
+    
+    graph = extractfield(result.hornSchunckNew.frameMean(:), 'AUROC_score');
+    figure
+    subplot(3, 1, 1)
+    title('Results for selected Dataset');
+    bar(1:size(graph,2), graph, 1)
+    hold on
+    plot(xlim, AUC_random, 'color', [0.5 0.5 0.5])
+    axis([0 size(graph,2)+1 AUC_min AUC_max])
+    set(gca, 'XTick', 1:size(graph,2))
+    ylabel('AUCROC');
+    clear data;
+    clear graph;
+    
+    graph = extractfield(result.hornSchunckNew.frameMean(:), 'KLDIV_score');
+    subplot(3, 1, 2)
+    title('Results for selected Dataset');
+    bar(1:size(graph,2), graph, 1)    
+    axis([0 size(graph,2)+1 KLDIV_min KLDIV_max])
+    set(gca, 'XTick', 1:size(graph,2))
+    ylabel('KLDIV');
+    clear data;
+    clear graph;
+    
+    graph = extractfield(result.hornSchunckNew.frameMean(:), 'NSS_score');
+    subplot(3, 1, 3)
+    title('Results for selected Dataset');
+    bar(1:size(graph,2), graph, 1)
+    axis([0 size(graph,2)+1 NSS_min NSS_max])
+    set(gca, 'XTick', 1:size(graph,2))
+    ylabel('NSS');
+    clear data;
+    clear graph;
+    
+    
 end
