170bbf192bd3fd926c73e0077b390b3d7ef4474a
Martin Kuchynar <matokuchy@gmail.com>
2016-04-24 15:31:03 +0200

    Fixing loading multiple videos.

--- src/prototype/findVideoByName.m
+++ src/prototype/findVideoByName.m
@@ -2,7 +2,7 @@ function result = findVideoByName(name, VideoData)
 
 result = 'Not found';
 for ii = 1:length(VideoData)
-   if (VideoData(ii).name == name)
+   if (strcmp(VideoData(ii).name, name))
        result = VideoData(ii);
    end
 end
