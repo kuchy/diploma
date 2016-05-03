c370ecc802b247e6b613bfcb389e42846786c1d0
Martin Kuchynar <matokuchy@gmail.com>
2016-05-03 20:18:54 +0200

    Fixing minimal time evaluation.

--- src/prototype/computeModel.m
+++ src/prototype/computeModel.m
@@ -9,8 +9,12 @@ imageNames = dir(fullfile(folder,'*.jpg'));
 imageNames = {imageNames.name}';
 evulationTimes= zeros(length(imageNames),1);
 
-for ii = 2:length(imageNames)
-    frameName = fullfile(folder,imageNames{ii-1});
+for ii = 1:length(imageNames)
+    if (ii~=1)
+        frameName = fullfile(folder,imageNames{ii-1});        
+    else
+        frameName = fullfile(folder,imageNames{1});
+    end
     oldFrame = imread(frameName);
     frameName = fullfile(folder,imageNames{ii});
     img = imread(frameName);
@@ -27,7 +31,7 @@ for ii = 2:length(imageNames)
     disp(['current frame:',num2str(ii),' time: ', num2str(time)]);   
 end
 
-averageTimeNum = mean2(evulationTimes);
+averageTimeNum = mean(evulationTimes);
 averageTime = num2str(averageTimeNum);
 maxTime = num2str(max(evulationTimes));
 minTime = num2str(min(evulationTimes));
